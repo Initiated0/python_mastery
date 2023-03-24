ef8250f7a3e6a2e22f7cc9b3911c584b3067dc26
Initiated0 <damonmayeehamm@gmail.com>
2023-03-24 17:19:06 -0400

    python docs

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ from pathlib import Path
 print("Home Dir: ", Path.home())
 
 path = Path("ecommerce/__init__.py")
-print(path.exists())
+print("is this the problem",path.exists())
 print(path.is_file())
 print(path.is_dir())
 
@@ -33,22 +33,33 @@ print("Parent ",path.parent)
 # path.rmdir() # removes directory
 # path.rename() # renames directory
 
-path = Path("ecommerce")
+path = Path("i_am_initiated")
 print("chechking if customer path exists: ", path.exists())
 
-
+print("this is directory iteration function: ")
 for p in path.iterdir():
     print(p)
+
+print("this is also directory iteration function: ")
+
 paths = [p for p in path.iterdir()]
 print("list: ", paths)
+
+print("this is directories inside directories function: ")
 
 paths = [p for p in path.iterdir() if p.is_dir()]
 print("list2: ", paths)
 
 #  to get the py files using glob method
 
-py_files = [p for p in path.glob("*.py")]
+py_files = [p for p in path.glob("*.txt")]
 print("Using glob: ",py_files)
 
 py_files = [p for p in path.rglob("*.py")]
 print("Using r - glob: ",py_files)
+
+# import shutil
+
+# source = Path("i_am_initiated/Anaconda.docx")
+# target = Path("i_am_initiated/dummyfolder/dummyfolder2/dummyfolder4")
+# shutil.copy(path, target)
